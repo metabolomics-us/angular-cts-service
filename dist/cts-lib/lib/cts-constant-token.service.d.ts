dd2f284cf1c194f01d56621c967f3cbfbb44796b
NolanGuzman97 <nolanguzman97@gmail.com>
2022-03-15 14:17:09 -0700

    committing new library files for the module

---
+++ dist/cts-lib/lib/cts-constant-token.service.d.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from '@angular/core';
+import { CtsConstant } from './cts-constant';
+export declare const CtsConstantTokenService: InjectionToken<CtsConstant>;
+//# sourceMappingURL=cts-constant-token.service.d.ts.map
---
+++ dist/cts-lib/lib/cts-constant.d.ts
@@ -0,0 +1,4 @@
+export interface CtsConstant {
+    apiUrl: 'https://cts.fiehnlab.ucdavis.edu';
+}
+//# sourceMappingURL=cts-constant.d.ts.map
